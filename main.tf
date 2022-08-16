b844f95917a63b0daf2e43ee8b68942a5fcd2689
Gerry Thome <gerry.thome@thinkahead.com>
2022-08-16 08:47:49 -0500

    changed CPU

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "vsphere_virtual_machine" "vm" {
   name             = "abc-demo"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 1
+  num_cpus         = 2
   memory           = 1024
   guest_id         = "centos7_64Guest"
   clone {
